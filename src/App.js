47017cf5469fb3319dc0aad037df5cdc7b8b52ef
Surya_teja_Duppanapudi <suryatej.duppanapudi@gmail.com>
2024-08-25 20:20:23 -0400

    Added email support

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import { Newsletter } from './components/Newsletter';
 
 function App() {
   return (
