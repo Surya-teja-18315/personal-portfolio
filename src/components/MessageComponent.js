8c33314e74d1ee4321300858652f7d2df4be2449
Surya_teja_Duppanapudi <suryatej.duppanapudi@gmail.com>
2024-08-26 22:31:07 -0400

    Update MessageComponent.js

--- src/components/MessageComponent.js
+++ src/components/MessageComponent.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import fetchMessage from "../utils/api";
+// import React, { useState, useEffect } from 'react';
+// import fetchMessage from "../utils/api";
 
-const MessageComponent = () => {
-    const [message, setMessage] = useState('');
+// const MessageComponent = () => {
+//     const [message, setMessage] = useState('');
 
-    useEffect(() => {
-        fetchMessage().then((data) => {
-            // Assuming the first item in the data array contains the text
-            if (data.length > 0) {
-                setMessage(data[0].text);
-            }
-        });
-    }, []);
+//     useEffect(() => {
+//         fetchMessage().then((data) => {
+//             // Assuming the first item in the data array contains the text
+//             if (data.length > 0) {
+//                 setMessage(data[0].text);
+//             }
+//         });
+//     }, []);
 
-    return (
-        <div>
-            <h1>Message from Backend:</h1>
-            <p>{message}</p>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <h1>Message from Backend:</h1>
+//             <p>{message}</p>
+//         </div>
+//     );
+// };
 
-export default MessageComponent;
+// export default MessageComponent;
